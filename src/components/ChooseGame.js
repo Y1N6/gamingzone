0eda193febe91d9c3bd56ab2c0e181df970a28d2
Yannick Lanteri <yannick-lanteri_student2021@wilder.school>
2022-01-20 20:44:16 +0100

    modify css on ChooseGame

--- src/components/ChooseGame.js
+++ src/components/ChooseGame.js
@@ -13,6 +13,18 @@ const ChooseGame = () => {
         <div>Prêt {localStorage.getItem('name')} ?</div>
         <div>Choisis un jeu</div>
       </div>
+      <div className="gamingList">
+        <div className="gamingList1">
+          <div className="game1">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/3/36/Mars_Valles_Marineris_EDIT.jpg" alt="gameOne"></img>
+            <p>Morpion</p>
+          </div>
+          <div className="game2">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/3/36/Mars_Valles_Marineris_EDIT.jpg" alt="gametwo"></img>
+            <p>Morpion</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
