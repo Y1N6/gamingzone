e9679c64da8b3c07431c7ec11613ffab19c9e3fb
MUNSCH <thomas-munsch_student2021@wilder.school>
2022-01-30 22:58:45 +0100

    numberGuess: added some style, game full functionnal, navigation between chogame and gamelist done

--- src/components/NumberGuess.js
+++ src/components/NumberGuess.js
@@ -1,14 +1,30 @@
 import Nav from "./Nav";
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 import "./styles/NumberGuess.css";
 
 const NumberGuess = () => {
+  let navigate = useNavigate();
   const [display, setDisplay] = useState("Choisi un numéro");
   const [userInput, setUserInput] = useState("");
+  const [answer, setAnswer] = useState(false);
+  const [hint, setHint] = useState("");
+  const [gameDone, setGameDone] = useState(false);
+  const [randomNbr, setRandomNbr] = useState(
+    Math.floor(Math.random() * 1000) + 1
+  );
+
+  console.log("random", randomNbr);
 
   const handleClickNum = (num) => {
-    setUserInput(userInput + num);
+    if (gameDone !== true) {
+      if (userInput.length < 1 && num == 0) {
+        return;
+      } else {
+        setUserInput(userInput + num);
+      }
+    }
   };
 
   //***********************  Supprimer le dernier élément d'une string (supprimer le dernier chiffre)  *************************/
@@ -16,18 +32,60 @@ const NumberGuess = () => {
     setUserInput(userInput.substring(0, userInput.length - 1));
   };
 
+  const handleClickValidate = () => {
+    console.log("Validé!");
+    setAnswer(+userInput);
+    setHint(false);
+  };
+
+  useEffect(() => {
+    console.log("setAnswer done", answer);
+
+    if (!answer) {
+      setDisplay("Choisi un numéro");
+    } else if (answer < randomNbr) {
+      setDisplay(`Ta réponse était: ${answer}`);
+      setHint(`C'est plus !`);
+    } else if (answer > randomNbr) {
+      setDisplay(`Ta réponse était: ${answer}`);
+      setHint(`C'est moins !`);
+    } else if (answer === randomNbr) {
+      setDisplay("Bravo !");
+      setGameDone(true);
+      setHint(false);
+    }
+    setUserInput("");
+  }, [answer]);
+
+  const restart = () => {
+    setGameDone(false);
+    setAnswer(false);
+    setRandomNbr(Math.floor(Math.random() * 1000) + 1);
+  };
+
   return (
     <div className="gameContainer">
-      {/* <Nav /> */}
-      <div className="numberGuessWrapper">
-        <button className="returnBtn" onClick={erase}>
+      <Nav />
+      <div className="returnBtnContainer">
+        <button className="returnBtn" onClick={() => navigate("/gameList")}>
           Retour
         </button>
+      </div>
+      <div className="numberGuessWrapper">
         <h1 className="numberGuessTitle">
-          Trouve le bon nombre entre 1 et 1000
+          Trouve le bon nombre
+          <br />
+          entre 1 et 1000
         </h1>
-        <div id="inputField">{userInput ? userInput : display}</div>
-        <div className="numberGuessOutput"></div>
+        <div className="restartArea">
+          {hint && <div id="hintArea">{hint}</div>}
+          {gameDone && (
+            <button id="restartBtn" onClick={restart}>
+              Rejouer
+            </button>
+          )}
+        </div>
+        <div id="outputField">{userInput ? userInput : display}</div>
         <ul className="digitWrapper">
           <div className="numberGuessContainer" id="digit1">
             <div
@@ -82,8 +140,18 @@ const NumberGuess = () => {
               0
             </div>
           </div>
+          <div id="eraseContainer">
+            <button className="eraseBtn" onClick={erase}>
+              &#9166;
+            </button>
+          </div>
           <div id="validateBtn">
-            <button className="numberGuessValidate">Valider</button>
+            <button
+              className="numberGuessValidate"
+              onClick={handleClickValidate}
+            >
+              Valider
+            </button>
           </div>
         </ul>
       </div>
